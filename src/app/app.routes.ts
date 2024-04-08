860ab32b2696138c3065d60f79df11b9377d9e9c
Amos <asidelnik@gmail.com>
2024-04-08 19:18:14 +0300

    routes - created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoanTypesComponent } from './routes/loan-types/loan-types.component';
+import { SubLoanTypesComponent } from './routes/sub-loan-types/sub-loan-types.component';
+import { LoanRequestsComponent } from './routes/loan-requests/loan-requests.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'loan-types', pathMatch: 'full' },
+  { path: 'loan-types', component: LoanTypesComponent },
+  { path: 'sub-loan-types', component: SubLoanTypesComponent },
+  { path: 'loan-requests', component: LoanRequestsComponent },
+  { path: '**', redirectTo: 'home' },
+];
