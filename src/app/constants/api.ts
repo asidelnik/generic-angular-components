b678a451b53e72342a727e20ed4c006779cd7167
Amos <asidelnik@gmail.com>
2024-04-08 20:37:22 +0300

    api constants

---
+++ src/app/constants/api.ts
@@ -0,0 +1,7 @@
+export const baseUrl = 'http://localhost:3000';
+
+export const serverPaths = {
+  loantTypes: '/loan-types/',
+  subLoanTypes: '/sub-loan-types/',
+  loanRequests: '/loan-requests/',
+};
