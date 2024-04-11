416664681e35c9691b0851bd1bc80bcd8c226c05
Amos <asidelnik@gmail.com>
2024-04-11 18:19:36 +0300

    Generic table fixed. Add pipe functionality to generic table component.

--- src/app/interfaces/tableColumn.ts
+++ src/app/interfaces/tableColumn.ts
@@ -1,6 +1,10 @@
+import { CurrencyPipe, DatePipe, DecimalPipe } from '@angular/common';
+
 export interface TableColumn {
   name: string;
   label: string;
   order: number;
   isSortable: boolean;
+  isPipe: boolean;
+  pipe: 'string' | 'date' | 'currency' | 'decimal';
 }
--- src/app/shared/components/generic-table/generic-table.component.ts
+++ src/app/shared/components/generic-table/generic-table.component.ts
@@ -1,12 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { TableColumn } from '../../../interfaces/tableColumn';
 import { DataUnionType } from '../../../interfaces/union';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {
+  CommonModule,
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+} from '@angular/common';
 
 @Component({
   selector: 'app-generic-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule, DatePipe],
   templateUrl: './generic-table.component.html',
   styleUrl: './generic-table.component.scss',
 })
@@ -15,12 +21,32 @@ export class GenericTableComponent implements OnInit {
   @Input() data: DataUnionType[] = [];
 
   displayedColumns: string[] = [];
-  dataSource: MatTableDataSource<DataUnionType> | undefined;
+  dataSource: MatTableDataSource<DataUnionType> = new MatTableDataSource();
+
+  constructor(
+    private datePipe: DatePipe,
+    private currencyPipe: CurrencyPipe,
+    private decimalPipe: DecimalPipe
+  ) {}
 
   ngOnInit() {
     this.displayedColumns = this.columns
       .sort((a, b) => a.order - b.order)
       .map((column) => column.name);
     this.dataSource = new MatTableDataSource(this.data);
+  }
+
+  transformData(column: TableColumn, data: any): any {
+    if (column.pipe === 'string') {
+      return data;
+    } else if (column.pipe === 'date') {
+      return this.datePipe.transform(data, 'shortDate');
+    } else if (column.pipe === 'currency') {
+      return this.currencyPipe.transform(data, 'USD');
+    } else if (column.pipe === 'decimal') {
+      return this.decimalPipe.transform(data, '1.2-2');
+    }
+
+    return data;
   }
 }
