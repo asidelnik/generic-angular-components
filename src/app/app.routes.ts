34342b83fd49068dc78f28a186e19eb9ae9ea438
Amos <asidelnik@gmail.com>
2024-04-12 12:25:46 +0300

    LifeCycleDemoComponent - added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { LoanTypesComponent } from './routes/loan-types/loan-types.component';
 import { SubLoanTypesComponent } from './routes/sub-loan-types/sub-loan-types.component';
 import { LoanRequestsComponent } from './routes/loan-requests/loan-requests.component';
+import { LifecycleDemoComponent } from './routes/lifecycle-demo/lifecycle-demo.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'loan-types', pathMatch: 'full' },
   { path: 'loan-types', component: LoanTypesComponent },
   { path: 'sub-loan-types', component: SubLoanTypesComponent },
   { path: 'loan-requests', component: LoanRequestsComponent },
+  { path: 'lifecycle-demo', component: LifecycleDemoComponent },
   { path: '**', redirectTo: 'home' },
 ];
---
+++ src/app/routes/lifecycle-demo/lifecycle-demo.component.ts
@@ -0,0 +1,70 @@
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  SimpleChanges,
+  Input,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './lifecycle-demo.component.html',
+  styleUrl: './lifecycle-demo.component.scss',
+})
+export class LifecycleDemoComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() inputProperty: any;
+
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges', changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+}
