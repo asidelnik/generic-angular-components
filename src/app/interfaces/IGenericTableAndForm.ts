e1e5965184a8d1d459a2a2cca4949c00bbf9678f
Amos <asidelnik@gmail.com>
2024-05-07 22:12:43 +0300

    Update interfaces for generic table and form

---
+++ src/app/interfaces/IGenericTableAndForm.ts
@@ -0,0 +1,46 @@
+export interface ITableColumn {
+  label: string;
+  name: string;
+  order: number;
+  isSortable: boolean;
+  pipe: 'string' | 'date' | 'currency' | 'decimal';
+}
+
+interface ITableFilterField {
+  label: string;
+  type: 'text' | 'select' | 'checkbox' | 'date' | 'number' | 'radio';
+  options?: string[];
+  value?: string;
+  required: boolean;
+  // dependentField?: string;
+  // dependentValue?: string;
+  // showIfDependentFieldValidated?: string;
+  // enableIfDependentFieldNotValidated?: string;
+}
+
+interface IFormField {
+  label: string;
+  type:
+    | 'text'
+    | 'select'
+    | 'checkbox'
+    | 'date'
+    | 'number'
+    | 'radio'
+    | 'textarea';
+  required: boolean;
+  options?: string[];
+  value?: string;
+  validators?: any[];
+}
+
+export interface IField {
+  type: 'number' | 'string' | 'boolean' | 'date';
+  tableColumn: ITableColumn;
+  formField: IFormField | null;
+  tableFilterField: ITableFilterField | null;
+}
+
+export type FieldsData<T> = {
+  [K in keyof T]: IField;
+};
--- src/app/interfaces/tableColumn.ts
+++
@@ -1,7 +0,0 @@
-export interface TableColumn {
-  name: string;
-  label: string;
-  order: number;
-  isSortable: boolean;
-  pipe: 'string' | 'date' | 'currency' | 'decimal';
-}
