5d67349ad82f1df7b2ca534d58544b6d10349621
Amos <asidelnik@gmail.com>
2024-05-14 12:15:59 +0300

    AddPanel - generic ReactForms - started

--- src/app/shared/components/add-panel/add-panel.component.ts
+++ src/app/shared/components/add-panel/add-panel.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IFormField } from '../../../interfaces/IGenericTableAndForm';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './add-panel.component.html',
-  styleUrl: './add-panel.component.scss'
+  styleUrl: './add-panel.component.scss',
 })
 export class AddPanelComponent {
+  @Input() formFields: IFormField[] = [];
+  form: FormGroup = new FormGroup({});
+  @Output() closePanel: EventEmitter<void> = new EventEmitter<void>();
+  constructor() {}
 
+  ngOnInit() {
+    this.formFields.forEach((field) => {
+      this.form.addControl(field.name, new FormControl(''));
+    });
+  }
+
+  onSubmit() {
+    this.closePanel.emit();
+  }
 }
